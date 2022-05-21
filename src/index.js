5200b58bc8004aa8037d248578ed1d12d701619f
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2022-05-21 16:11:41 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,83 @@
+let newtext;
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+$("#border_top_left_radius").change(function(){
+    let border_top_left_radius = $(this).val();
+    $( ".box" ).css( "border-top-left-radius", `${border_top_left_radius}%`);
+    $(".box-container" ).css("border-top-left-radius", `${border_top_left_radius}%`);
+
+    let tl = $( ".box" ).css( "border-top-left-radius");
+    $("#border_top_left_radius").val(parseInt(tl));
+    $("#tl-lab").text(`Top Left: ${parseInt(tl)}%`);
+
+    newtext = $(".box" ).css("border-radius");
+    $("#css-output" ).text(`border-radius: ${newtext};`);
+});
+
+$("#border_top_right_radius").change(function(){
+    let border_top_right_radius = $(this).val();
+    $( ".box" ).css( "border-top-right-radius", `${border_top_right_radius}%`);
+    $(".box-container" ).css("border-top-right-radius", `${border_top_right_radius}%`);
+
+    let tr = $(".box" ).css( "border-top-right-radius");
+    $("#border_top_right_radius").val(parseInt(tr));
+    $("#tr-lab").text(`Top Right: ${parseInt(tr)}%`);
+    newtext = $(".box" ).css("border-radius");
+    $("#css-output" ).text(`border-radius: ${newtext};`);
+});
+
+$("#border_bottom_right_radius").change(function(){
+    let border_bottom_right_radius = $(this).val();
+    $( ".box" ).css( "border-bottom-right-radius", `${border_bottom_right_radius}%`);
+    $(".box-container" ).css("border-bottom-right-radius", `${border_bottom_right_radius}%`);
+
+    let br = $( ".box" ).css( "border-bottom-right-radius");
+    $("#border_bottom_right_radius").val(parseInt(br));
+    $("#br-lab").text(`Bottom Right: ${parseInt(br)}%`);
+
+    newtext = $(".box" ).css("border-radius");
+    $("#css-output" ).text(`border-radius: ${newtext};`);
+});
+
+$("#border_bottom_left_radius").change(function(){
+    let border_bottom_left_radius = $(this).val();
+    $( ".box" ).css( "border-bottom-left-radius", `${border_bottom_left_radius}%`);
+    $(".box-container" ).css("border-bottom-left-radius", `${border_bottom_left_radius}%`);
+
+    let bl = $(".box" ).css( "border-bottom-left-radius");
+    $("#border_bottom_left_radius").val(parseInt(bl));
+    $("#bl-lab").text(`Bottom Left: ${parseInt(bl)}%`);
+
+    newtext = $(".box" ).css("border-radius");
+    $("#css-output" ).text(`border-radius: ${newtext};`);
+});
+
+$("#radius-genrator").click(function(){
+
+    $(".box" ).css( "border-bottom-left-radius", `${randomIntFromInterval(0, 100)}%`);
+    let bl = $(".box" ).css( "border-bottom-left-radius");
+    $("#border_bottom_left_radius").val(parseInt(bl));
+    $("#bl-lab").text(`Top Left: ${parseInt(bl)}%`);
+
+    $( ".box" ).css( "border-bottom-right-radius", `${randomIntFromInterval(0, 100)}%`);
+    let br = $( ".box" ).css( "border-bottom-right-radius");
+    $("#border_bottom_right_radius").val(parseInt(br));
+    $("#br-lab").text(`Top Left: ${parseInt(br)}%`);
+
+    $(".box" ).css( "border-top-right-radius", `${randomIntFromInterval(0, 100)}%`);
+    let tr = $(".box" ).css( "border-top-right-radius");
+    $("#border_top_right_radius").val(parseInt(tr));
+    $("#tr-lab").text(`Top Left: ${parseInt(tr)}%`);
+
+    $( ".box" ).css( "border-top-left-radius", `${randomIntFromInterval(0, 100)}%`);
+    let tl = $( ".box" ).css( "border-top-left-radius");
+    $("#border_top_left_radius").val(parseInt(tl));
+    $("#tl-lab").text(`Top Left: ${parseInt(tl)}%`);
+
+    newtext = $(".box" ).css("border-radius");
+    $(".box-container" ).css("border-radius", `${newtext}`);
+    $("#css-output" ).text(`border-radius: ${newtext};`);
+});
